a768e5d9e679bb776b611c4249c09085807efa91
Алексей <95961917+ARibleTeam@users.noreply.github.com>
2025-05-16 09:22:38 +0300

    Delete Django/staticfiles directory

--- Django/staticfiles/js/start-menu.js
+++
@@ -1,9 +0,0 @@
-const loadApp = (slug) => {
-    const appLinks = document.querySelectorAll('.app-box');
-    appLinks.forEach(link => link.classList.remove('active')); // снимаем выделение со всех
-
-    const appBox = document.querySelector(`.app-box[data-slug="${slug}"]`);
-    appBox.classList.add('active'); // подсвечиваем активное приложение
-
-    window.location.href = `/chat/${slug}/`;
-};
